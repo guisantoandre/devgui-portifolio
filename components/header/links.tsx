9483571ec222ba2b0154602ff81ca03b9eefe9e0
guisantoandre <gui.santoandre@gmail.com>
2023-09-23 08:09:16 -0300

    ajuste tamanho botao menu mobile

--- components/header/links.tsx
+++ components/header/links.tsx
@@ -72,7 +72,7 @@ export function Links({ onClick, open, onClose }: LinksProps) {
       // md:flex-row md:items-center
       <nav className="relative flex flex-col justify-between gap-y-9 md:flex-row md:items-center">
          <div
-            className="absolute -top-[32px] -left-[76px] bg-customblue-100 backdrop-blur-sm rounded-tl-[4px] rounded-bl-[4px] p-3 cursor-pointer select-none md:hidden"
+            className="absolute -top-[32px] -left-[67px] bg-customblue-100 backdrop-blur-sm rounded-tl-[4px] rounded-bl-[4px] px-2 py-3 cursor-pointer select-none md:hidden"
             onClick={() => onClick()}
          >
             <img
